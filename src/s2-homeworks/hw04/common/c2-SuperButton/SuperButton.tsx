25a8b281202d8ac4cacd02b2e10f87af40372147
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-23 15:17:20 +0300

    hw04

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,7 +19,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button
     + (disabled
-            ? ' ' + disabled
+            ? ' ' + s.disabled
             : xType === 'red'
                 ? ' ' + s.red
                 : xType === 'secondary' ? ' ' + s.secondary : '' + s.default)
